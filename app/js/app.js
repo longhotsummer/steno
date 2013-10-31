a514aa702d196066ace1aeb79a889740fb5aeed0
Greg Kempe <greg@kempe.net>
2013-10-31 17:46:32 +0200

    Add ACE editor.

--- app/js/app.js
+++ app/js/app.js
@@ -1,21 +1,39 @@
-$(function() {
-  var refresh = function(data) {
-    // refresh the form
-    console.log(data);
-    $('#doc_source_text').val(data.source_text);
-    $('#doc_xml').val(data.xml);
-  };
+var Steno = {
+  sourceTextEd: null,
 
-  $('#form').on('submit', function(e) {
+  init: function() {
+    $('#form').on('submit', Steno.submitForm);
+
+    var ed;
+    Steno.sourceTextEd = ed = ace.edit("doc_source_text");
+
+    ed.setTheme("ace/theme/chrome");
+    ed.setShowPrintMargin(false);
+
+    var sess = ed.getSession();
+    sess.setMode("ace/mode/text");
+    sess.setUseWrapMode(false);
+  },
+
+  // refresh the forms based on data received from the server
+  update: function(data) {
+    console.log(data);
+
+    Steno.sourceTextEd.setValue(data.source_text);
+    $('#doc_xml').val(data.xml);
+  },
+
+  submitForm: function(e) {
     try {
       $('#parse_btn').addClass('disabled');
 
+      data = $(this).serializeArray();
+      data.push({name: 'doc[source_text]', value: Steno.sourceTextEd.getValue()});
+
       $.ajax('/parse', {
         method: 'POST',
-        data: $(this).serializeArray(),
-        success: function(data, text) {
-          refresh(data);
-        },
+        data: data,
+        success: Steno.update,
         complete: function() {
           $('#parse_btn').removeClass('disabled');
         }
@@ -25,5 +43,9 @@ $(function() {
     }
 
     return false;
-  });
+  }
+};
+
+$(function() {
+  Steno.init();
 });
