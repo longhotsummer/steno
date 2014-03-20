2f2874ccee3e746d2563907774218caccee5ce74
Greg Kempe <greg@kempe.net>
2014-03-20 13:15:15 +0200

    Warn before navigating away from the page.

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -4,5 +4,9 @@
 $(function() {
   window.steno = new Steno.App();
   window.steno.init();
-});
 
+  $(window).on('beforeunload', function(e) {
+    e.preventDefault();
+    return 'You will lose your changes!';
+  });
+});
