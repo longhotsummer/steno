54b9188fe0d2e12c63ca19fa215692aeb3bb7e4c
Greg Kempe <greg@kempe.net>
2014-07-04 11:38:35 +0200

    Include pdftotext as a 64bit binary

--- lib/steno/importer.rb
+++ lib/steno/importer.rb
@@ -1,7 +1,11 @@
 require 'open3'
 
+require 'logging'
+
 module Steno
   class Importer
+    include Logging
+
     def import_from_upload(mimetype, file)
       case mimetype
       when "text/plain"
@@ -12,10 +16,19 @@ module Steno
     end
 
     def import_from_pdf(file)
-      data = file.read()
-      stdout, status = Open3.capture2('pdftotext - -', stdin_data: data)
+      cmd = "#{Importer.pdftotext_path} #{file.path} -"
+      logger.info("Executing: #{cmd}")
+      stdout, status = Open3.capture2(cmd)
 
       status == 0 ? stdout : nil
+    end
+
+    def self.pdftotext_path
+      @@pdftotext_path
+    end
+
+    def self.pdftotext_path=(val)
+      @@pdftotext_path = val
     end
   end
 end
